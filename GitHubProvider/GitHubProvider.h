6f7d36ac8d347b97c4f3c4d485e2f6a70cd11d7b
Angel Morales <a.jesse3001@gmail.com>
2018-01-04 16:45:36 -0600

    initial commit

---
+++ GitHubProvider/GitHubProvider.h
@@ -0,0 +1,19 @@
+//
+//  GitHubProvider.h
+//  GitHubProvider
+//
+//  Created by Angel Jesse Morales Karam Kairuz on 04/01/18.
+//  Copyright © 2018 TheKairuz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GitHubProvider.
+FOUNDATION_EXPORT double GitHubProviderVersionNumber;
+
+//! Project version string for GitHubProvider.
+FOUNDATION_EXPORT const unsigned char GitHubProviderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GitHubProvider/PublicHeader.h>
+
+
